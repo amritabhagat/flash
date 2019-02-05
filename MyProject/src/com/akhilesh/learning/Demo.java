87dd1329d13d7ce5d9a17bebaa03e50927f9411f
Akhilesh <abhagat@finfort.ind.in>
2019-02-06 03:15:09 +0530

    demo 3

--- MyProject/src/com/akhilesh/learning/Demo.java
+++ MyProject/src/com/akhilesh/learning/Demo.java
@@ -6,5 +6,6 @@ public static void main(String[] args) {
 	System.out.println("Hello world");
 	System.out.println("Git world");
 	System.out.println("Git world");
+	System.out.println("my new GIT project");
 }
 }
