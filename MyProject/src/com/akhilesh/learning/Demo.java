012520bd53086881d99bdb70126a3db3249be60e
Akhilesh <abhagat@finfort.ind.in>
2019-02-06 02:59:51 +0530

    demo 2

--- MyProject/src/com/akhilesh/learning/Demo.java
+++ MyProject/src/com/akhilesh/learning/Demo.java
@@ -3,6 +3,8 @@ package com.akhilesh.learning;
 public class Demo {
 public static void main(String[] args) {
 	System.out.println("Hello world");
+	System.out.println("Hello world");
+	System.out.println("Git world");
 	System.out.println("Git world");
 }
 }
